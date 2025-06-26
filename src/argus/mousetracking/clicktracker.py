585a4ebd3989a8b16bb127ebc21c16988e6d9a9f
Phantom-VK <vikramadityakhupse@gmail.com>
2025-06-26 19:53:43 +0530

    Updated inactivity tracking

--- src/argus/mousetracking/clicktracker.py
+++ src/argus/mousetracking/clicktracker.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pynput import mouse
 import threading
+from src.argus.logger import logging
 
 class ClickTracker:
     def __init__(self, inactivity_threshold=1800):  # 30 minutes
@@ -8,26 +9,31 @@ class ClickTracker:
         self.inactivity_threshold = inactivity_threshold
         self.callback = None
         self.listener = None
+        self.monitor_activity = False
 
     def on_click(self, x, y, button, pressed):
         if pressed:
             self.last_click_time = datetime.now()
-            if self.callback:
-                self.callback(active=True)  # Notify of activity
+            logging.info(f"Mouse clicked at {self.last_click_time}")
+            self.callback(activity=True)  # Notify of activity
 
-    def start_monitoring(self, callback=None):
-        self.callback = callback
+    def start_monitoring(self):
         self.listener = mouse.Listener(on_click=self.on_click)
         self.listener.start()
+        self.last_click_time = datetime.now()
+        logging.info("ClickTracker started")
+        self.monitor_activity  = True
         threading.Thread(target=self._monitor_inactivity, daemon=True).start()
 
     def _monitor_inactivity(self):
-        while True:
+        while self.monitor_activity:
             time_since_last_click = (datetime.now() - self.last_click_time).total_seconds()
+            logging.info(f"TIme since last click {time_since_last_click}")
             if time_since_last_click > self.inactivity_threshold and self.callback:
-                self.callback(active=False)  # Notify of inactivity
+                self.callback(activity = False)  # Notify of inactivity
             threading.Event().wait(10)  # Check every 10 seconds
 
     def stop_monitoring(self):
         if self.listener:
             self.listener.stop()
+            self.monitor_activity = False
