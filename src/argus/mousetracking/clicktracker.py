64f5a9b539ccfc1ddc50757e1c119d5e94c1bb40
Phantom-VK <vikramadityakhupse@gmail.com>
2025-06-26 23:36:20 +0530

    Fixed resume-pause race condition while paused

--- src/argus/mousetracking/clicktracker.py
+++ src/argus/mousetracking/clicktracker.py
@@ -5,6 +5,7 @@ from src.argus.logger import logging
 
 class ClickTracker:
     def __init__(self, inactivity_threshold=1800):  # 30 minutes
+        self.is_paused = False
         self.last_click_time = datetime.now()
         self.inactivity_threshold = inactivity_threshold
         self.callback = None
@@ -14,7 +15,7 @@ class ClickTracker:
     def on_click(self, x, y, button, pressed):
         if pressed:
             self.last_click_time = datetime.now()
-            logging.info(f"Mouse clicked at {self.last_click_time}")
+            print(f"Mouse clicked at {self.last_click_time}")
             self.callback(activity=True)  # Notify of activity
 
     def start_monitoring(self):
@@ -28,10 +29,15 @@ class ClickTracker:
     def _monitor_inactivity(self):
         while self.monitor_activity:
             time_since_last_click = (datetime.now() - self.last_click_time).total_seconds()
-            logging.info(f"Time since last click {time_since_last_click}")
-            if time_since_last_click > self.inactivity_threshold and self.callback:
-                self.callback(activity = False)  # Notify of inactivity
-            threading.Event().wait(10)  # Check every 10 seconds
+            print(f"Time since last click {time_since_last_click}")
+
+            # Only trigger inactivity if we're currently active
+            if (time_since_last_click > self.inactivity_threshold
+                    and self.callback
+                    and not self.is_paused):  # Add this condition
+                self.callback(activity=False)
+
+            threading.Event().wait(10)
 
     def stop_monitoring(self):
         if self.listener:
